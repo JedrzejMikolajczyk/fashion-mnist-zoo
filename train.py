59d1d0355347ecc1661753590c177ba73485b644
JedrzejMikolajczyk <jedrek.mikolajczyk.zs@gmail.com>
2022-06-27 23:26:33 +0100

    added accepting arguments from command line

--- train.py
+++ train.py
@@ -10,63 +10,67 @@ import torch.optim as optim
 import torchvision
 from torchvision import datasets, transforms
 
-from models.net1 import Net1
+#from models.net1 import Net1
+
+from models.cnn import CNN
+
 import utils
 
-seed = 42
-split = 0.8
-labels = ['t_shirt_top', 'trouser', 'pullover', 'dress', 'coat', 'sandal', 'shirt', 'sneaker', 'bag', 'ankle_boots']
+
+import argparse
 
 
-transform = transforms.Compose([transforms.ToTensor(),
-                                transforms.Normalize((0.5), (0.5))])
-#Load the training data
-dataset = torchvision.datasets.FashionMNIST(root= './data', train = True, transform=transform, download = True)
-
-
-trainset_size = int(split *len(dataset))
-validset_size = len(dataset) - trainset_size
-
-trainset, validset = torch.utils.data.random_split(dataset, [trainset_size, validset_size], generator=torch.Generator().manual_seed(seed))
-
-supersample = trainset[0][0]
-batch_size = 64
-
-print(trainset, validset)
-print(trainset[0][0].shape)
-
-
-# Create data loaders
-train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size)
-test_loader = torch.utils.data.DataLoader(validset, batch_size=4, 
-                                         shuffle=False)
-
-print(train_loader, test_loader)
-
-device = 'cuda' if torch.cuda.is_available() else "cpu"
-
-
-
-
-epochs = 100
-
-model = Net1()
-
-
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-
-val_loss = 999999
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    utils.train(train_loader, model, loss_fn, optimizer, device)
+if __name__ == '__main__':
     
-    current_val_loss = utils.test(test_loader, model, loss_fn, device)
-    if current_val_loss < val_loss:
-        val_loss = current_val_loss
-        torch.save(model.state_dict(), "model1.pth")
-        print("Model1 saved")
-print("Done!", val_loss)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--n_epochs", type=int, default=10, help="number of epochs of training")
+    parser.add_argument("--ratio", type=float, default=0.8, help="portion of samples that is used for training (remaining part used for validation during training)")
+    parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
+    parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
+    args = parser.parse_args()
+
+
+    seed = 42
+    split = args.ratio
+    labels = ['t_shirt_top', 'trouser', 'pullover', 'dress', 'coat', 'sandal', 'shirt', 'sneaker', 'bag', 'ankle_boots']
+    
+    
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Normalize((0.5), (0.5))])
+    #Load the training data
+    dataset = torchvision.datasets.FashionMNIST(root= './data', train = True, transform=transform, download = True)
+    
+    
+    trainset_size = int(split *len(dataset))
+    validset_size = len(dataset) - trainset_size
+    
+    trainset, validset = torch.utils.data.random_split(dataset, [trainset_size, validset_size], generator=torch.Generator().manual_seed(seed))
+    
+    # Create data loaders
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size)
+    test_loader = torch.utils.data.DataLoader(validset, batch_size=4, 
+                                             shuffle=False)
+    
+    
+    device = 'cuda' if torch.cuda.is_available() else "cpu"
+    
+    model = CNN()
+    
+    
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr)
+    
+    val_loss = 999999
+    for t in range(args.n_epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        utils.train(train_loader, model, loss_fn, optimizer, device)
+        
+        current_val_loss = utils.test(test_loader, model, loss_fn, device)
+        if current_val_loss < val_loss:
+            val_loss = current_val_loss
+            torch.save(model.state_dict(), "ttt1.pth")
+            print("Model1 saved")
+    print("Done!", val_loss)
 
 
 
